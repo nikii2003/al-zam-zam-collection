cfc8252e661903b286a3843553ca125e2be4ebd6
Nikita kadiwike <ninavenikita492@gmail.com>
2023-10-26 18:35:19 +0530

    added signup and login

--- server/index.js
+++ server/index.js
@@ -6,49 +6,80 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 const connectDB = async ()=>{
     const conn = await mongoose.connect(process.env.MONGODB_URI)
     if (conn){
         console.log('mongodb connected successfuly')
     }
 }
-connectDB();
+// POST /signup 
 
-app.get('/users', async (req,res)=>{
-    const users= await User.find();
-    res.json({
-    success:true,
-    data:users,
-    message:" all info "
-    })
-})
-app.post('/signup', async (req,res)=>{
-    const{name,email,password,mobile,address,gender}=req.body;
-     
-    const newUser = new User({
-     name,
-     email,
-     password,
-     mobile,
-     address,
-     gender
-    })
-   try {
-    const savenewUser = await newUser.save();
-    res.json({
-      success:true,
-      data:savenewUser,
-      message:"successfully add products"
-    })
-   } catch (error) {
-    
-   }
-})
+app.post("/signup", async (req, res) => {
+    const { name, email, password, mobile, address, gender } = req.body;
+  
+    const user = new User({
+      name: name,
+      email: email,
+      password: password,
+      mobile: mobile,
+      address: address,
+      gender: gender,
+    });
+  
+    try {
+      const savedUser = await user.save();
+  
+      res.json({
+        success: true,
+        data: savedUser,
+        message: "signup successfully..."
+      });
+    } catch (err) {
+      res.json({
+        success: false,
+        message: err.message,
+      });
+    }
+  });
 
+  // POST / Login
+
+app.post ('/login', async (req,res)=>{
+    const {email,password} =req.body;
+    if(!email || !password){
+      return res.json({
+        success : false,
+        message : "please provide email and password"
+        })
+    }
+
+    const user = await User.findOne({
+        email : email,
+        password : password
+    })
+
+    if(user){
+        return res.json({
+            success : true,
+            data:user,
+            message :"login successful"
+        })
+    }else{
+        return res.json({
+            success : false,
+            message :"invalid creadentials"
+        }) 
+    }
+  
+})  
 const PORT = process.env.PORT || 5000 ;
 
 app.listen(PORT,()=>{
     console.log(`server is running on PORT ${PORT}`)
+    connectDB();
 })
+
 
 
--- server/modules/User.js
+++ server/modules/User.js
@@ -1,14 +1,33 @@
 import { Schema,model } from "mongoose";
 
-const userSchema = new Schema ({
-  name:String,
-  email:String,
-  password:String,
-  mobile:String,
-  address:String,
-  gender:String
+const userSchema = new  Schema({
+  name :{
+    type : String,
+    default : "-",
+  },
+  email : {
+    type : String,
+    required : true,
+    unique : true,
+  },
+  password : {
+    type : String ,
+    required : true,
+  },
+  mobile : {
+    type : String ,
+    required : true,
+    unique : true, 
+  },
+  address : {
+    type : String ,
+  },
+  gender : {
+    type : String,
+    default : "prefer not to say.."
+  }
 })
 
-const User = model('User', userSchema)
+const User = model("User", userSchema)
 
 export default User
