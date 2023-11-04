05163c740118674419fd4d353518eee06676236c
Nikita kadiwike <ninavenikita492@gmail.com>
2023-11-04 22:48:10 +0530

    login api fetch

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Login.css"
+import axios from 'axios';
 
 function Login() {
+  const [email, setEmail] = useState ();
+  const [password,setPassword] = useState ();
+
+  const login =  async ()=>{
+    const response = await axios.post("/login", {
+      email : email,
+      password : password
+    })
+    if(response?.data?.success ){
+      alert(response?.data?.message)
+      window.location.href = "/"
+    }
+  }
   return (
-    <div>Login</div>
+    <div>
+      <div className='login-continer'>
+        <form>
+          <h1 className='text-center'>Login</h1>
+          <div className='div-input-field'>
+            <label htmlFor='email'>Email:</label>
+            <input type='email' 
+            placeholder='Enter email' 
+            id='email' 
+            value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}
+            className='input-box input-box-container'/>
+        </div>
+        <div className='div-input-field'>
+            <label htmlFor='password'>password:</label>
+            <input type='password' 
+            placeholder='Enter password' 
+            id='password' 
+            value={password}
+            onChange={(e)=>{setPassword(e.target.value)}}
+            className='input-box input-box-container'/>
+        </div>
+
+        <div>
+          <button type='button' className='btn login-btn' onClick={login}>Login</button>
+        </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
